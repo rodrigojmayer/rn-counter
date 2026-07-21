de1bfe702b9fb756b7ce8ef81d554290453be72d
rodrigojmayer <rodrigojmayer@gmail.com>
2026-07-21 17:45:38 -0300

    drawer navigator implementation

---
+++ rm-counter-app/src/navigation/DrawerNavigator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import RegistrarEventoScreen from '../screens/RegistrarEventoScreen';
+import HistorialCompletoScreen from '../screens/HistorialCompletoScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Registrar"
+      screenOptions={{
+        drawerPosition: 'left',
+        drawerType: 'front',
+        headerStyle: { backgroundColor: '#34495e' },
+        headerTintColor: '#fff',
+        drawerActiveTintColor: '#2ecc71',
+        drawerInactiveTintColor: '#7f8c8d'
+      }}
+    >
+      <Drawer.Screen
+        name="Registrar"
+        component={RegistrarEventoScreen}
+        options={{ title: 'Registrar Evento' }}
+      />
+      <Drawer.Screen
+        name="HistorialCompleto"
+        component={HistorialCompletoScreen}
+        options={{ title: 'Historial Completo' }}
+      />
+    </Drawer.Navigator>
+  );
+}
